1e21e44968a9ba93b5f10ebc10ed17026718358d
Markus Peröbner <markus.peroebner@gmail.com>
2019-11-27 13:27:09 +0100

    add documentation

--- demo.js
+++ demo.js
@@ -1,8 +1,15 @@
 let { map, takeUntil } = rxjs.operators;
 
+// defines a custom component. the second argument is a binding callback
+// which will be invoked once for every component instance. events
+// contains observables for all events which are produced within the
+// component. they are determined from the component's template.
 rxweb.define('my-comp', events => {
     let counter = new rxjs.BehaviorSubject(0);
+    // return output observables which are accessed within the template.
     return {
+        // the _ output has a special meaning. it will be subscribed
+        // while the component is within the document.
         _: events.increaseCounter
             .pipe(map(([inc, $event]) => {
                 $event.preventDefault();

3f7c01d06ec0a9a08bfa3995c74ea678a5f3aa88
Markus Peröbner <markus.peroebner@gmail.com>
2020-01-21 21:49:55 +0100

    add rxweb-text-content tests

--- rxweb.spec.js
+++ rxweb.spec.js
@@ -1,5 +1,7 @@
 describe('rxweb', () => {
-    let templateContainer, contentContainer;
+    let { BehaviorSubject, of } = rxjs;
+    
+    let templateContainer, contentContainer, element;
     beforeAll(() => {
         templateContainer = document.createElement('div');
         document.body.appendChild(templateContainer);
@@ -16,10 +18,55 @@ describe('rxweb', () => {
     describe('hello-world component', () => {
         beforeAllAddTemplate('hello-world', '<h1>Hello World!</h1>');
         beforeAllDefineComponent('hello-world', events => ({}));
-        beforeEachInstantiateComponent('hello-world');
+        beforeAllInstantiateComponent('hello-world');
         
         it('should have text content "Hello World!"', () => {
             expect(contentContainer.textContent).toBe('Hello World!');
+        });
+    });
+
+    describe('rxweb-text-content static', () => {
+        beforeAllAddTemplate('rxweb-text-content-static-comp', '<span rxweb-text-content="myInput"></span>');
+        beforeAllDefineComponent('rxweb-text-content-static-comp', events => ({
+            myInput: of('some text'),
+        }));
+        beforeAllInstantiateComponent('rxweb-text-content-static-comp');
+
+        it('should render some text', () => {
+            expect(contentContainer.textContent).toBe('some text');
+        });
+    });
+
+    describe('rxweb-text-content dynamic', () => {
+        let myInput;
+        beforeAll(() => {
+            myInput = new BehaviorSubject('');
+        });
+        afterAll(() => {
+            myInput = undefined;
+        });
+        beforeAllAddTemplate('rxweb-text-content-dynamic-comp', '<span rxweb-text-content="myInput"></span>');
+        beforeAllDefineComponent('rxweb-text-content-dynamic-comp', events => ({
+            myInput: myInput.asObservable(),
+        }));
+        beforeAllInstantiateComponent('rxweb-text-content-dynamic-comp');
+
+        beforeEach(() => {
+            myInput.next('initial');
+        });
+
+        it('should render initial', () => {
+            expect(contentContainer.textContent).toBe('initial');
+        });
+
+        describe('change myInput', () => {
+            beforeEach(() => {
+                myInput.next('changed');
+            });
+
+            it('should render changed', () => {
+                expect(contentContainer.textContent).toBe('changed');
+            });
         });
     });
 
@@ -38,13 +85,13 @@ describe('rxweb', () => {
         });
     }
 
-    function beforeEachInstantiateComponent(componentName){
+    function beforeAllInstantiateComponent(componentName){
         let element;
-        beforeEach(() => {
+        beforeAll(() => {
             element = document.createElement(componentName);
             contentContainer.appendChild(element);
         });
-        afterEach(() => {
+        afterAll(() => {
             contentContainer.removeChild(element);
             element = undefined;
         });

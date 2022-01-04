46c7ee6c5542d5ccedc3b33e67c9a6b302f039e1
Markus Peröbner <markus.peroebner@gmail.com>
2022-01-04 19:05:19 +0100

    add npm release script + more metadata

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+version="$1"
+if [[ -z "${version}" ]]
+then
+    echo "Usage: $0 VERSION" >&2
+    exit 1
+fi
+package_version=`jq -r .version package.json `
+if [[ "${version}" != "${package_version}" ]]
+then
+    echo "package.json contains version ${package_version}" >&2
+    exit 1
+fi
+npm run build
+git tag "${version}"
+npm publish --dry-run
+git push origin "${version}"
